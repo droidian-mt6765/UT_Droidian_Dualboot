ee0d69068b8a249c1967df0520af34844a89575a
Bardia Moshiri <fakeshell@bardia.tech>
2022-06-29 21:36:55 +0430

    update dualboot repo for dandelion and angelica

--- boot_switch.sh
+++ boot_switch.sh
@@ -1,18 +1,16 @@
 #!/bin/sh
 
-bootdevices=$(getprop ro.boot.boot_devices)
-
 if grep -s '/data ' /proc/mounts; then
    true
 else
-   mount -t ext4 /dev/block/platform/$bootdevices/by-name/userdata /data
+   mount -t ext4 /dev/block/platform/bootdevice/by-name/userdata /data
 fi
 
 if [ -f /data/.BOOT_UT ]; then
-   dd if=/data/boot_ut.img of=/dev/block/platform/$bootdevices/by-name/boot
+   dd if=/data/boot_ut.img of=/dev/block/platform/bootdevice/by-name/boot
    rm /data/.BOOT_UT
 elif [ -f /data/.BOOT_DROIDIAN ]; then
-   dd if=/data/boot_droidian.img of=/dev/block/platform/$bootdevices/by-name/boot
+   dd if=/data/boot_droidian.img of=/dev/block/platform/bootdevice/by-name/boot
    rm /data/.BOOT_DROIDIAN
 else
    true
